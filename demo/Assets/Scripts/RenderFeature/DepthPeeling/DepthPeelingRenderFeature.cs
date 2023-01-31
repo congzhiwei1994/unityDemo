7c0aee7ea6e59ae1e788bb5014dffc614c42a37c
congzhiwei1994 <75684067+congzhiwei1994@users.noreply.github.com>
2023-01-31 21:04:28 +0800

    11111

---
+++ demo/Assets/Scripts/RenderFeature/DepthPeeling/DepthPeelingRenderFeature.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
+
+namespace czw.DepthPeeling
+{
+    public class DepthPeelingRenderFeature : ScriptableRendererFeature
+    {
+        [System.Serializable]
+        public class Setting
+        {
+            public RenderPassEvent _event = RenderPassEvent.AfterRenderingOpaques;
+            [Range(0, 10)] public int passNumber = 5;
+            public string passTags = "DepthPeeling";
+            public RenderQueueType renderQueueType = RenderQueueType.Transparent;
+            public SortingCriteria sortingCriteria = SortingCriteria.CommonOpaque;
+        }
+
+        public Setting setting = new Setting();
+        private DepthPeelingRenderPass pass;
+
+        public override void Create()
+        {
+            pass = new DepthPeelingRenderPass(setting);
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            pass.renderPassEvent = setting._event;
+            renderer.EnqueuePass(pass);
+        }
+    }
+}
